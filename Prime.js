7409375a7ce9f3ae30ac096364f109525639d102
Rehan Shekh <rehanrshekh@gmail.com>
2022-04-25 18:27:09 +0530

    JavaScript Leap Year Practice Problem

---
+++ Prime.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+// program to check leap year
+function checkLeapYear(year) {
+
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = (prompt('Enter a year:'));
+
+checkLeapYear(year);
+
+
